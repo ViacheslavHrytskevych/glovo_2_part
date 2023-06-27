a129319cf66cc1715a8a781d3afc62ae4092d158
Viacheslav Hrytskevych <hrytskevych.v@gmail.com>
2023-06-27 18:20:31 +0300

    Initial commit

---
+++ src/main/java/com/example/glovo_2_part/Glovo2PartApplication.java
@@ -0,0 +1,13 @@
+package com.example.glovo_2_part;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Glovo2PartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Glovo2PartApplication.class, args);
+    }
+
+}
